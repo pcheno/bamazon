29d24fa4d7611762aafec52e12aec8ae0a6724f5
Paul Cheno <pwchenoweth@gmail.com>
2018-04-17 14:06:52 -0500

    added main menu, working out bugs

--- bamazonManager.js
+++ bamazonManager.js
@@ -14,72 +14,35 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-
 function allProduct() {
+    console.log("\033c");
     connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log("\n" + "+".repeat(50) + "\n" + res[i].product_name + "\nItem Id: " + res[i].item_id + "\nDepartment: " + res[i].department_name + "\nPrice: " + "$" + res[i].price + "\nQuantity: " + res[i].stock_quantity);
         }
-    });
+    }); //query
 } //end allProduct
 
 function lowProduct() {
+    console.log("\033c");
     connection.query("SELECT * FROM products WHERE stock_quantity < 5 ORDER BY stock_quantity ASC", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log("\n" + "+".repeat(50) + "\n" + res[i].product_name + "\nItem Id: " + res[i].item_id + "\nDepartment: " + res[i].department_name + "\nPrice: " + "$" + res[i].price + "\nQuantity: " + res[i].stock_quantity);
         }
-    });
+    }); //query
 } //end lowProduct
 
 
-
-function addProduct() {
-    inquirer.prompt([{
-            name: "addId",
-            message: "Enter the Id of the Item you would like to add: "
-        },
-        {
-            name: "prod_name",
-            message: "Enter product name: "
-        },
-        {
-            name: "dept",
-            message: "Enter department name: "
-        },
-        {
-            name: "price",
-            message: "Enter price: $"
-        },
-        {
-            name: "quantity",
-            message: "quantity: "
-        }
-    ]).then(function (input) {
-
-        var query = connection.query("INSERT INTO products SET ?", {
-            item_id: input.addId,
-            product_name: input.prod_name,
-            department_name: input.dept,
-            price: input.price,
-            stock_quantity: input.quantity
-        }, function (err, res) {
-            if (err) {
-                console.log("\nDuplicate item id, unable to add");
-            } else {
-                console.log("\nItem added")
-            }
-            // connection.end();
-        }); //query
-    });
-} //function addproduct
-
 function addInventory() {
+    console.log("\033c");
     inquirer.prompt([{
             name: "id",
+            type: "input",
             message: "Enter the Id of the Item you would like to add inventory: "
         },
         {
             name: "quantity",
+            type: "input",
             message: "How many to add to stock quantity: "
         }
     ]).then(function (userIn) {
@@ -95,17 +58,108 @@ function addInventory() {
                         item_id: userIn.id
                     }
                 ]);
-                console.log("Item " + userIn.id +" quantity updated " + sum);
+                console.log("Item " + userIn.id + " quantity updated " + sum);
             }
-
-            connection.end();
         }); //query
-    });
+    }); //.then
+} // function addInventory
+
+function addProduct() {
+    console.log("\033c");
+    inquirer.prompt([{
+            name: "addId",
+            type: "input",
+            message: "Enter the Id of the Item you would like to add: "
+        },
+        {
+            name: "prod_name",
+            type: "input",
+            message: "Enter product name: "
+        },
+        {
+            name: "dept",
+            type: "input",
+            message: "Enter department name: "
+        },
+        {
+            name: "price",
+            type: "input",
+            message: "Enter price: $"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "quantity: "
+        }
+    ]).then(function (input) {
+
+        var query = connection.query("INSERT INTO products SET ?", {
+            item_id: input.addId,
+            product_name: input.prod_name,
+            department_name: input.dept,
+            price: input.price,
+            stock_quantity: input.quantity
+        }, function (err, res) {
+            if (err) {
+                console.log("\nUnable to add, Id not valid, Remember no duplicates\n");
+            } else {
+                console.log("\nItem added\n")
+            }
+            mainMenu();
+        }); //query
+    }); //.then
+} //function addProduct
+
+function exitProgram() {
+    //console.log("\033c");
+    console.log("\ngood bye")
+    connection.end();
 }
 
-/////////////////////////////
+function mainMenu() {
+    inquirer.prompt([{
+        name: "action",
+        type: "rawlist",
+        message: "Choose Action?",
+        choices: [
+            "View Products for Sale",
+            "View Low Inventory < 5",
+            "Add to Inventory",
+            "Add New Product",
+            "Exit Menu"
+        ]
+    }]).then(function (answer) {
+        switch (answer.action) {
+            case "View Products for Sale":
+                allProduct();
+                break;
+            case "View Low Inventory < 5":
+                lowProduct();
+                break;
+            case "Add to Inventory":
+                addInventory();
+                break;
+            case "Add New Product":
+                addProduct();
+                break;
+            case "Exit Menu":
+                console.log("\033c");
+                console.log("\ngood bye")
+                connection.end();
+                break;
+            default:
+                console.log("\033c");
+                console.log("\nEnter valid input\n")
+                mainMenu();
+        } //switch
+    }); //.then
+} //function mainMenu
+
+
+
+/////////////Starts Here////////////
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    addInventory();
+    console.log("\033c");
+    mainMenu();
 });
